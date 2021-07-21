6aa4048e940212f7f3ef7255ab19397ef7a388c7
torontotoky <nickolai.bel1aev@yandex.ru>
2021-07-21 20:43:40 +0300

    dsad

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,35 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:todolistt/widgets/bottom_bar.dart';
+import 'package:todolistt/widgets/top_bar.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            Container(
-              child: FloatingActionButton(
-                child: Icon(Icons.add),
-                onPressed: () {}, //Добавить задание
-                elevation: 5,
-                tooltip: 'Добавить новую задачу',
-              ),
-              padding: EdgeInsets.fromLTRB(0.0, 0.0, 20.0, 10.0),
-            ),
-            BottomBar(),
-          ],
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Container(
+          height: 50.0,
+          child: BottomBar(),
         ),
       ),
-      /* floatingActionButton: FloatingActionButton(
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {}, //Добавить задание
         elevation: 10,
         tooltip: 'Добавить новую задачу',
-      ),*/
+      ),
     );
   }
 }
--- lib/widgets/top_bar.dart
+++ lib/widgets/top_bar.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 
 class TopBar extends StatelessWidget {
-  const TopBar({Key? key}) : super(key: key);
-
+  //DateTime day = new DateTime.now();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          border: Border(top: BorderSide(width: 1.0, color: Colors.grey)),
+          color: Colors.red,
+        ),
+        height: 50,
+        padding:
+            EdgeInsets.only(left: 25.0, right: 25.0, bottom: 2.0, top: 5.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(children: [Drawer()]),
+            // Column(children: [Text(day.toString())])
+          ],
+        ));
   }
 }
